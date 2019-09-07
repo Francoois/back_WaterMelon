2836138704e4d1492b01d177dc0cc5f0d570f3dd
Francoois <francoois.louis@gmail.com>
2019-09-07 22:38:25 +0200

    reference database file

--- res/database/database.sql
+++ res/database/database.sql
@@ -1,26 +1,60 @@
-create table users (
+create table users 
+(
   id bigint(20) unsigned not null AUTO_INCREMENT,
-  first_name VARCHAR(255) NOT NULL,
-  last_name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  is_admin BOOLEAN NOT NULL,
-  api_key VARCHAR(64),
-  PRIMARY KEY (id)
+  first_name varchar(255) not null,
+  last_name varchar(255) not null,
+  email varchar(255) UNIQUE not null,
+  password varchar(255) not null,
+  is_admin boolean not null,
+  api_key varchar(64),
+  primary key (id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE wallets (
-  id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id BIGINT(20) UNSIGNED NOT NULL,
-  PRIMARY KEY (id)
+create table wallets
+(
+  id bigint(20) unsigned not null AUTO_INCREMENT,
+  user_id bigint(20) unsigned not null,
+  primary key (id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE cards (
-  id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  user_id BIGINT(20) UNSIGNED NOT NULL,
-  last_4 CHAR(4) NOT NULL,
-  brand ENUM('visa', 'master_card', 'american_express', 'union_pay', jcb) NOT NULL,
-  expired_at DATE NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users (id)
-)
+create table cards
+(
+  id bigint(20) unsigned not null AUTO_INCREMENT,
+  user_id bigint(20) unsigned not null,
+  last_4 char(4) not null,
+  brand ENUM('visa', 'master_card', 'american_express', 'union_pay', 'jcb') not null,
+  expired_at date not null,
+  primary key (id)
+) ENGINE=InnoDB;
+
+create table payins
+(
+  id bigint(20) unsigned not null AUTO_INCREMENT,
+  wallet_id bigint(20) unsigned not null,
+  amount bigint(20) not null,
+  primary key (id)
+) ENGINE=InnoDB;
+
+create table payouts
+(
+  id bigint(20) unsigned not null AUTO_INCREMENT,
+  wallet_id bigint(20) unsigned not null,
+  amount bigint(20) not null,
+  primary key (id)
+) ENGINE=InnoDB;
+
+create table transfers
+(
+  id bigint(20) unsigned not null AUTO_INCREMENT,
+  debited_wallet_id bigint(20) unsigned not null,
+  credited_wallet_id bigint(20) unsigned not null,
+  amount bigint(20) not null,
+  primary key (id)
+) ENGINE=InnoDB;
+
+ALTER TABLE `wallets` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `cards` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+ALTER TABLE `payins` ADD FOREIGN KEY (`wallet_id`) REFERENCES `wallets` (`id`);
+ALTER TABLE `payouts` ADD FOREIGN KEY (`wallet_id`) REFERENCES `wallets` (`id`);
+ALTER TABLE `transfers` ADD FOREIGN KEY (`debited_wallet_id`) REFERENCES `wallets` (`id`);
+ALTER TABLE `transfers` ADD FOREIGN KEY (`credited_wallet_id`) REFERENCES `wallets` (`id`);
