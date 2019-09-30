524c0c7bc7c77bf6e5d0ac4d5dd7a3859ac7eb95
Francoois <francoois.louis@gmail.com>
2019-09-30 20:00:47 +0200

    CREATE/READ test OK on users

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -3,6 +3,19 @@ var request = require("request");
 let base_url = `http://localhost:8000`;
 
 let testTime = Date.now();
+let addedUserId,
+userAttributes = {
+  header : {'Content-Type' : 'application/x-www-form-urlencoded'},
+  url : base_url+"/users",
+  form : {
+    first_name:"jasmin",
+    last_name:"testor",
+    email:"jasmin"+testTime+"@wanadoo.fr",
+    password:"passsword",
+    api_key:"lakeeeeeey",
+    is_admin:false
+  }
+};
 
 describe("Users API OK", function() {
      describe("GET /users", function() {
@@ -14,26 +27,34 @@ describe("Users API OK", function() {
             });
          });
     });
+    describe("POST /users & GET new user", function() {
+        it("returns status code 200 twice", function(done) {
+            new Promise(
+              function(resolve,reject){
+                request.post(
+                userAttributes,
+                function(error, response, body) {
+                  addedUserId = parseInt(response.body);
+                  //console.log(addedUserId);
+                  expect(response.statusCode).toBe(200);
+                  resolve();
+                  //done();
+                });
+              }
+            ).then(()=>{
+              request.get(base_url+"/users/"+addedUserId, function(error, response, body){
+              expect(response.statusCode).toBe(200);
 
-    describe("POST /users", function() {
-        it("returns status code 200", function(done) {
-            request.post(
-              {
-                header : {'Content-Type' : 'application/x-www-form-urlencoded'},
-                url : base_url+"/users",
-                form : {
-                  first_name:"jasmin",
-                  last_name:"testor",
-                  email:"jasmin"+testTime+"@wanadoo.fr",
-                  password:"passsword",
-                  api_key:"lakeeeeeey",
-                  is_admin:false
-                }
-              },
-              function(error, response, body) {
-                expect(response.statusCode).toBe(200);
-                done();
-              });
+              let testUserAttributes = JSON.parse(JSON.stringify(userAttributes.form));
+              testUserAttributes.id=addedUserId;
+
+              let testBody = JSON.parse(response.body)[0];
+              testBody.is_admin=new Boolean(testBody.is_admin);
+
+              expect(testBody).toEqual(testUserAttributes);
+              done();
+            });
+          });
         });
    });
 });
