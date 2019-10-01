9fbc6c0cefeb6df72d5988bcf6d63b304902f54f
Francoois <francoois.louis@gmail.com>
2019-10-01 20:50:46 +0200

    requirejs working

--- app.js
+++ app.js
@@ -1,160 +1,158 @@
-const express = require('express'); //https://expressjs.com/fr/api.html#res
+const requirejs = require('requirejs');
 
-const bodyParser = require('body-parser');
-
-// Password encryption https://github.com/kelektiv/node.bcrypt.js#readme
-const bcrypt = require('bcrypt');
-
-let db = require('./src/data/dbConnector.js');
-let users = require(`./src/model/users`);
-
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//////// Users route
-app.get('/users', function(req, res) {
-  let query = `SELECT * FROM users`;
-  db.queryDB(query).then(
-    (result)=>{res.status(200).json(result)},
-    ()=>{res.sendStatus(500)});
-});
-app.post('/users', function(req, res) {
-  users.create(req);
+requirejs.config({
+  baseUrl : 'src/',
+  nodeRequire: require
 });
 
-app.get('/users/:id(\\d+)', function(req, res){
-  const query = `SELECT * FROM users WHERE  id=${req.params.id}`;
-  db.queryDB(query
-  ).then(
-    (result)=>{res.status(200).json(result)}
-  ).catch(
-    ()=>{res.sendStatus(400)}
-  );
-});
+requirejs([
+  'express', //https://expressjs.com/fr/api.html#res
+  'body-parser',
+  'bcrypt', // Password encryption https://github.com/kelektiv/node.bcrypt.js#readme
+  'data/dbConnector'],
 
-app.put('/users/:id(\\d+)', function(req, res){
-  const query = _updateQueryBuilder("users",req);
-  queryDB(query,res);
-});
-/**
-* Handles Users AND Wallet DELETE
-*/
-app.delete('/users/:id(\\d+)', function(req, res){
-  let id = req.params.id;
-  let query = `DELETE FROM users WHERE id=${id}`;
-  let queryWallet = `DELETE FROM wallets WHERE user_id=${id}`;
+function(express, bodyParser, bcrypt, db){
 
-  queryDB(queryWallet);
-  queryDB(query, res);
-  // XXX : create real dependance and triggers
-});
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-/*
-///////////////////////// Cards Route
-app.get('/cards', function(req, res) {
-  let query = `SELECT * FROM cards`;
-  queryDB(query, res);
-});
-app.get('/cards/:id(\\d+)', function(req, res) {
-  let query = `SELECT * FROM cards WHERE id=${req.params.id}`;
-  queryDB(query, res);
-});
-app.post('/cards', function(req,res){
-  const query = _insertQueryBuilder("cards", req);
-  queryDB(query, res);
-});
-app.put('/cards/:id(\\d+)', function(req,res){
-  const query = _updateQueryBuilder("cards",req);
-  queryDB(query, res);
-});
-app.delete('/cards/:id(\\d+)', function(req, res){
-  let id = req.params.id;
-  let query = `DELETE FROM cards WHERE id=${id}`;
-  queryDB(query,res);
-});
-
-
-app.get('/wallets', function(req, res) {
-  let query = `SELECT * FROM wallets`;
-  queryDB(query,res);
-});
-
-app.get('/payins', function(req, res) {
-  let query = `SELECT * FROM payins`;
-  queryDB(query, res);
-});
-function _createPayIn(req,res){
-  let queryPayin = _insertQueryBuilder("payins",{
-    body : {
-      wallet_id : req.body.credited_wallet_id,
-      amount : req.body.amount
-    }
+  //////// Users route
+  app.get('/users', function(req, res) {
+    let query = `SELECT * FROM users`;
+    db.queryDB(query).then(
+      (result)=>{res.status(200).json(result)},
+      ()=>{res.sendStatus(500)});
   });
-  return queryDB(queryPayin, res, true);
-}
-
-app.get('/payouts', function(req, res) {
-  let query = `SELECT * FROM payouts`;
-  queryDB(query, res);
-});
-function _createPayout(req,res){
-  let queryPayOut = _insertQueryBuilder("payouts",{
-    body : {
-      wallet_id : req.body.debited_wallet_id,
-      amount : req.body.amount
-    }
+  app.post('/users', function(req, res) {
+    users.create(req);
   });
-  return queryDB(queryPayOut,res,true);
-}
 
-app.get('/transfers', function(req, res) {
-  let query = `SELECT * FROM transfers`;
-  queryDB(query, res);
-});
-app.get('/transfers/:id(\\d+)', function (req,res){
-  _getTransferById(req.params.id,res);
-});
-app.post('/transfers', function(req, res){
-  let queryTransfer = _insertQueryBuilder("transfers", req);
-  queryDB(queryTransfer);
-
-  let payInProm=_createPayIn(req);
-  let payOutProm=_createPayout(req);
-
-  Promise.all([payInProm, payOutProm])
-  .then(
-    result => { res.sendStatus(200); })
-    .catch(() => { sendError(res); });
-});
-
-app.delete('/transfers', function (req,res){
-  //TODO check right to delete
-  let delPayIProm = _deletePayin(req,res);
-  let delPayOProm = _deletePayout(req,res);
-  Promise.all([delPayIProm,delPayOProm])
-  .then(
-    ()=>{ _deleteById("transfers", req.params.id);}
-  )
-  .then( ()=>{sendOk(res);})
-  .catch( ()=>sendError(res));
-});
-*/
-app.listen(8000, function(){
-  db.connection.connect(function(err){
-    if (err) throw err;
-    console.log('Connection to database successful!');
+  app.get('/users/:id(\\d+)', function(req, res){
+    const query = `SELECT * FROM users WHERE  id=${req.params.id}`;
+    db.queryDB(query
+    ).then(
+      (result)=>{res.status(200).json(result)}
+    ).catch(
+      ()=>{res.sendStatus(400)}
+    );
   });
-  console.log('Example app listening on port 3000!');
+
+  app.put('/users/:id(\\d+)', function(req, res){
+    const query = _updateQueryBuilder("users",req);
+    queryDB(query,res);
+  });
+  /**
+  * Handles Users AND Wallet DELETE
+  */
+  app.delete('/users/:id(\\d+)', function(req, res){
+    let id = req.params.id;
+    let query = `DELETE FROM users WHERE id=${id}`;
+    let queryWallet = `DELETE FROM wallets WHERE user_id=${id}`;
+
+    queryDB(queryWallet);
+    queryDB(query, res);
+    // XXX : create real dependance and triggers
+  });
+
+  /*
+  ///////////////////////// Cards Route
+  app.get('/cards', function(req, res) {
+    let query = `SELECT * FROM cards`;
+    queryDB(query, res);
+  });
+  app.get('/cards/:id(\\d+)', function(req, res) {
+    let query = `SELECT * FROM cards WHERE id=${req.params.id}`;
+    queryDB(query, res);
+  });
+  app.post('/cards', function(req,res){
+    const query = _insertQueryBuilder("cards", req);
+    queryDB(query, res);
+  });
+  app.put('/cards/:id(\\d+)', function(req,res){
+    const query = _updateQueryBuilder("cards",req);
+    queryDB(query, res);
+  });
+  app.delete('/cards/:id(\\d+)', function(req, res){
+    let id = req.params.id;
+    let query = `DELETE FROM cards WHERE id=${id}`;
+    queryDB(query,res);
+  });
+
+
+  app.get('/wallets', function(req, res) {
+    let query = `SELECT * FROM wallets`;
+    queryDB(query,res);
+  });
+
+  app.get('/payins', function(req, res) {
+    let query = `SELECT * FROM payins`;
+    queryDB(query, res);
+  });
+  function _createPayIn(req,res){
+    let queryPayin = _insertQueryBuilder("payins",{
+      body : {
+        wallet_id : req.body.credited_wallet_id,
+        amount : req.body.amount
+      }
+    });
+    return queryDB(queryPayin, res, true);
+  }
+
+  app.get('/payouts', function(req, res) {
+    let query = `SELECT * FROM payouts`;
+    queryDB(query, res);
+  });
+  function _createPayout(req,res){
+    let queryPayOut = _insertQueryBuilder("payouts",{
+      body : {
+        wallet_id : req.body.debited_wallet_id,
+        amount : req.body.amount
+      }
+    });
+    return queryDB(queryPayOut,res,true);
+  }
+
+  app.get('/transfers', function(req, res) {
+    let query = `SELECT * FROM transfers`;
+    queryDB(query, res);
+  });
+  app.get('/transfers/:id(\\d+)', function (req,res){
+    _getTransferById(req.params.id,res);
+  });
+  app.post('/transfers', function(req, res){
+    let queryTransfer = _insertQueryBuilder("transfers", req);
+    queryDB(queryTransfer);
+
+    let payInProm=_createPayIn(req);
+    let payOutProm=_createPayout(req);
+
+    Promise.all([payInProm, payOutProm])
+    .then(
+      result => { res.sendStatus(200); })
+      .catch(() => { sendError(res); });
+  });
+
+  app.delete('/transfers', function (req,res){
+    //TODO check right to delete
+    let delPayIProm = _deletePayin(req,res);
+    let delPayOProm = _deletePayout(req,res);
+    Promise.all([delPayIProm,delPayOProm])
+    .then(
+      ()=>{ _deleteById("transfers", req.params.id);}
+    )
+    .then( ()=>{sendOk(res);})
+    .catch( ()=>sendError(res));
+  });
+  */
+  app.listen(8000, function(){
+    db.connect(function(err){
+      if (err) throw err;
+      console.log('Connection to database successful!');
+    });
+    console.log('Example app listening on port 3000!');
+  });
+
 });
-
-
-
-function sendError(res){
-  res.sendStatus(500);
-}
-function sendOk(res){
-  res.sendStatus(200);
-}
 
 // TODO : Tests
 // TODO : Classes / Modules
--- src/data/dbConnector.js
+++ src/data/dbConnector.js
@@ -1,36 +1,13 @@
 //dbConnector.js
 
-const mysql = require('mysql');
+define(['mysql'], function(mysql){
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  user: "admin",
-  password: "admin",
-  database: "watermelon",
-  port: "3306"
-});
-
-// TODO : remove parameters from sendReq with appropriate `this` trick
-/**
-* Returns a Promise to execute the query
-* query : the string query to execute
-* res[optional] : response object to send the response with
-*
-*/
-function queryDB(query){
-  return new Promise( function(resolve,reject){
-    console.log(query)
-    connection.query(query, function(err, result, fields) {
-      if (err){
-        //throw err
-        console.error("WaterMelonDB Error :" + err);
-        reject();
-      }else{
-        console.log("success");
-        resolve(result);
-      }
+  return mysql.createConnection({
+      host: "localhost",
+      user: "admin",
+      password: "admin",
+      database: "watermelon",
+      port: "3306"
     });
-  });
-}
 
-module.exports = {connection, queryDB};
+});
