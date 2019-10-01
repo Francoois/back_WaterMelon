2017b50d1dead76d47ea00ecc3b2f8ae32951e7e
Francoois <francoois.louis@gmail.com>
2019-10-01 21:08:55 +0200

    Singleton dbConnector

--- src/data/dbConnector.js
+++ src/data/dbConnector.js
@@ -2,12 +2,38 @@
 
 define(['mysql'], function(mysql){
 
-  return mysql.createConnection({
-      host: "localhost",
-      user: "admin",
-      password: "admin",
-      database: "watermelon",
-      port: "3306"
-    });
+  let instance = null;
+
+  function MySingleton(){
+    if(instance !== null){
+        throw new Error("Cannot instantiate more than one MySingleton, use MySingleton.getInstance()");
+    }
+
+    this.initialize();
+  }
+  MySingleton.prototype = {
+      initialize: function(){
+          // summary:
+          //      Initializes the singleton.
+
+          this.db = mysql.createConnection({
+                host: "localhost",
+                user: "admin",
+                password: "admin",
+                database: "watermelon",
+                port: "3306"
+              });
+      }
+  };
+  MySingleton.getInstance = function(){
+      // summary:
+      //      Gets an instance of the singleton. It is better to use
+      if(instance === null){
+          instance = new MySingleton();
+      }
+      return instance;
+  };
+
+  return MySingleton.getInstance().db;
 
 });
